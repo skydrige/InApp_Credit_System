89424aee69a46c18f7f97dfbed18562b725efb28
skydrige <klokeshklokesh42376@gmail.com>
2024-05-03 01:20:30 +0530

    Regrouping

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { Form, Button } from 'react-bootstrap';
+import {addWalletListener, getCurrentWalletConnected} from "./Handle";
 
 function Register({ onSwitch }) {
+    const [walletAddress, setWalletAddress] = useState('');
+    
+    useEffect(() => {
+        getCurrentWalletConnected().then(address => setWalletAddress(address));
+        addWalletListener(setWalletAddress);
+    }, []);
+    
     return (
         <Form className="auth-form">
             <h3 className="text-center mb-4">Register Here</h3>
             <Form.Group>
                 <Form.Label>Username</Form.Label>
                 <Form.Control type="text" placeholder="Username"/>
-            </Form.Group>
-            <Form.Group>
-                <Form.Label>Wallet Address</Form.Label>
-                <Form.Control type="text" placeholder="Address"/>
             </Form.Group>
             <Form.Group>
                 <Form.Label>Password</Form.Label>
@@ -21,10 +25,13 @@ function Register({ onSwitch }) {
                 <Form.Label>Confirm Password</Form.Label>
                 <Form.Control type="password" placeholder="Confirm Password"/>
             </Form.Group>
-            <Button variant="primary" type="submit" className="w-100 mt-4">
+            <Button type="button" className="w-100 mt-4" disabled={!walletAddress}>
+                {walletAddress ? 'Wallet Connected' : 'Connect Wallet'}
+            </Button>
+            <Button type="submit" className="w-100 mt-4">
                 Register
             </Button>
-            <Button variant="secondary" onClick={onSwitch} className="w-100 mt-3">
+            <Button onClick={onSwitch} className="w-100 mt-3">
                 Go to Login
             </Button>
         </Form>
